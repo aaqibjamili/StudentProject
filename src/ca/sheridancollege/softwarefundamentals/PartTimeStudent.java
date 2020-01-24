f1416d6a1d2f997aa41de90df93283b050fbce93
aaqibjamili <jamilim@sheridancollege.ca>
2020-01-24 11:39:31 -0500

    Initialize commit StudentProject

---
+++ src/ca/sheridancollege/softwarefundamentals/PartTimeStudent.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.softwarefundamentals;
+
+/**
+ *
+ * @author Muhammad
+ */
+public class PartTimeStudent extends Student {
+   
+       private int hoursOfStudy;
+              public PartTimeStudent (String givenName, String givenID);
+              {
+                      super(givenName, givenID);
+              }
+            
+ 
+}
